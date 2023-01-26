eaddff51fad10295267c0b04281d0633f07a242b
Huzaif_ <huzaifahmed0@gmail.com>
2023-01-26 11:22:50 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//  IMPROTS
+const express = require("express");
+
+
+//  CONSTANTS
+const PORT = 3769;
+
+
+const app = express();
+
+//  YOUR CODE GOES HERE
+
+
+//  YOUR CODE ENDS HERE
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
